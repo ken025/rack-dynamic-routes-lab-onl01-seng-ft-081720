83dc232b82bac084a8ec42696c474a1d2e1415ac
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-23 02:59:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application
  
    if req.path.match(/items/)
  
-      item_name = req.path.split("/items/").last
-      item = @@items.detect { |i| i.name == item_name }
+      item_names = req.path.split("/items/").last
+      item = @@items.find{|s| s.title == item_names}
    if item
       resp.write item.price
      else
